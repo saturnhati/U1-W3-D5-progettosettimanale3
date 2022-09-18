933cc85ac5ef93bd93e0cc4b92be219fc1aa8f7c
saturnhati <76071008+saturnhati@users.noreply.github.com>
2022-09-18 21:34:34 +0200

    fixed project && finished

--- with eval/assets/js/script.js
+++ with eval/assets/js/script.js
@@ -1,16 +1,22 @@
-let newOp;
-let symbol;
-let last;
+let newOp
+let symbol = false
+let error
 
 function writeToOutput(element) {
+  if (document.getElementById('output').innerHTML == 'ERROR') {
+    console.log('errore')
+    document.getElementById('output').innerHTML = ''
 
-  last = document.getElementById('output').innerHTML.charAt(document.getElementById('output').innerHTML.length - 1);
-
-  if ( last !== '*' && last !== '/' && last !== '+' && last !== '-' ) {
-    if (element == '*' || element == '/' || element == '+' || element == '-'){
+  }
+  if (symbol == true && element == '*' || symbol == true && element == '/' || symbol == true && element == '+' || symbol == true && element == '-') {
+    document.getElementById('output').innerHTML = 'ERROR'
+    symbol = false
+  }
+  else {
+    if (element == '*' || element == '/' || element == '+' || element == '-') {
       newOp = false
+      symbol = true
     }
-        
     if (newOp == true) {
       document.getElementById('output').innerHTML = ''
       document.getElementById('output').innerHTML += element
@@ -20,36 +26,27 @@ function writeToOutput(element) {
       document.getElementById('output').innerHTML += element
     }
   }
-  else {
-    document.getElementById('output').innerHTML += ''
-    console.log('errore')
-  }
-
+  console.log(symbol)
+  console.log(newOp)
 }
-  
 
-
-
-  
 function showResults() {
-    newOp = true;
-    symbol = false;
-    let result = eval(document.getElementById('output').innerHTML)
-    console.log(result)
-    console.log(typeof result)
-    
+  newOp = true
+  symbol = false
+  let result = eval(document.getElementById('output').innerHTML)
+  console.log(result)
+  console.log(typeof result)
+
 
   if (isNaN(result) == true) {
-      document.getElementById('output').innerHTML = 'ERROR'
-   }
-   else {
-      document.getElementById('output').innerHTML = result
+    document.getElementById('output').innerHTML = 'ERROR'
+  }
+  else {
+    document.getElementById('output').innerHTML = result
   }
 }
-  
-function deleteOutput() {
-  document.getElementById('output').innerHTML = '' 
-}
 
-document.getElementById('output').innerHTML
+function deleteOutput() {
+  document.getElementById('output').innerHTML = ''
+}
 
